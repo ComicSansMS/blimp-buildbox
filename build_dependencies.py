54e14f6b505a8ce0c4a31cf394a0fd2fd0cdef0c
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2019-06-24 19:57:28 +0200

    added aws-sdk-cpp

--- build_dependencies.py
+++ build_dependencies.py
@@ -22,7 +22,7 @@ sys.stdout.flush()
 build_dir = os.path.join(cwd, "aws-c-common-build")
 source_dir = os.path.join(cwd, "aws-c-common")
 call(["cmake", "-G", cmake_generator, "-A", cmake_architecture,
-      "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir ],
+      "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir],
     cwd=build_dir)
 for c in configs:
     call(["cmake", "--build", build_dir, "--config", c, "-j"])
@@ -37,7 +37,7 @@ sys.stdout.flush()
 build_dir = os.path.join(cwd, "aws-checksums-build")
 source_dir = os.path.join(cwd, "aws-checksums")
 call(["cmake", "-G", cmake_generator, "-A", cmake_architecture,
-      "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir ],
+      "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir],
     cwd=build_dir)
 for c in configs:
     call(["cmake", "--build", build_dir, "--config", c, "-j"])
@@ -52,11 +52,28 @@ sys.stdout.flush()
 build_dir = os.path.join(cwd, "aws-c-event-stream-build")
 source_dir = os.path.join(cwd, "aws-c-event-stream")
 call(["cmake", "-G", cmake_generator, "-A", cmake_architecture,
-      "-DCMAKE_INSTALL_PREFIX=" + install_dir, "-DCMAKE_PREFIX_PATH=", install_dir, source_dir ],
+      "-DCMAKE_INSTALL_PREFIX=" + install_dir, "-DCMAKE_PREFIX_PATH=" + install_dir, source_dir],
     cwd=build_dir)
 for c in configs:
     call(["cmake", "--build", build_dir, "--config", c, "-j"])
     if (do_run_tests): call(["cmake", "--build", build_dir, "--config", c, "--target", "RUN_TESTS"])
+    call(["cmake", "--build", build_dir, "--config", c, "--target", "INSTALL"])
+
+print("*******************************************************************************")
+print("***  aws-cpp-sdk                                                            ***")
+print("*******************************************************************************")
+sys.stdout.flush()
+
+build_dir = os.path.join(cwd, "aws-sdk-cpp-build")
+source_dir = os.path.join(cwd, "aws-sdk-cpp")
+call(["cmake", "-G", cmake_generator, "-A", cmake_architecture,
+      "-DCMAKE_INSTALL_PREFIX=" + install_dir, "-DCMAKE_PREFIX_PATH=" + install_dir,
+      "-DBUILD_DEPS=OFF", "-DBUILD_SHARED_LIBS=OFF", "-DENABLE_UNITY_BUILD=ON", "-DENABLE_TESTING=OFF",
+      "-DBUILD_ONLY=core;glacier", source_dir],
+    cwd=build_dir)
+for c in configs:
+    call(["cmake", "--build", build_dir, "--config", c, "-j"])
+    #if (do_run_tests): call(["cmake", "--build", build_dir, "--config", c, "--target", "RUN_TESTS"])
     call(["cmake", "--build", build_dir, "--config", c, "--target", "INSTALL"])
 
 print("*******************************************************************************")
